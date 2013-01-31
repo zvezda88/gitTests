1d8e5960411a686af58484a1a50fb40e595c0b27
VictoriaP <Victoria@Victoria-PC.(none)>
2013-01-31 12:57:48 +0200

    New files and a change

---
+++ Person.java
@@ -0,0 +1,15 @@
+
+public class Person implements Actions {
+
+	private String name;
+	private String Id;
+	
+	public Person(String name, String Id) {
+		this.name = name;
+		this.Id = Id;
+	}
+	
+	public void work() {
+		
+	}
+}
